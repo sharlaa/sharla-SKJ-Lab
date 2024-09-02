71b018c7f7a7b07d4d077a72c940958a1ada89bb
sharlaa <121415756+sharlaa@users.noreply.github.com>
2024-09-02 21:30:17 +0700

    Create multiply.cpp

---
+++ Assignment1/multiply.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1 = 5;
+    int num2 = 10;
+    int result = 0;
+
+    result = num1 * num2;
+
+    cout << "The result is: " << result << endl;
+
+    return 0;
+}
